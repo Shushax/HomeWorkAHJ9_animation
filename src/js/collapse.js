8f98d940dc669c6e9076e1b7ae9a2ba4acf22e1e
Ksenia Sidorova <nekfi@mail.ru>
2020-12-26 17:11:54 +0300

    Fix

--- src/js/collapse.js
+++ src/js/collapse.js
@@ -1,21 +1,22 @@
-export default class Collapse {
-    constructor(text) {
-        this.text = text;
-    }
+/* eslint no-unused-expressions:0 */ // --> OFF
 
-    doButton() {
-        const btn = document.createElement('button');
-        btn.classList.add('btn');
-        btn.textContent = 'Collapse'
-        document.body.insertAdjacentElement('beforeEnd', btn);
-        const text = document.createElement('p');
-        text.classList.add('text');
-        text.textContent = this.text;
-        btn.insertAdjacentElement('afterend', text);
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            text.classList.contains('active') ? text.classList.remove('active') : text.classList.add('active');
-            
-        })
-    }
+export default class Collapse {
+  constructor(text) {
+    this.text = text;
+  }
+
+  doButton() {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.textContent = 'Collapse';
+    document.body.insertAdjacentElement('beforeEnd', btn);
+    const text = document.createElement('p');
+    text.classList.add('text');
+    text.textContent = this.text;
+    btn.insertAdjacentElement('afterend', text);
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      text.classList.contains('active') ? text.classList.remove('active') : text.classList.add('active');
+    });
+  }
 }
