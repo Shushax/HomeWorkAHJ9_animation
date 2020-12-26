1a530de8c04da582537e8aacca6c5146238087de
Ksenia Sidorova <nekfi@mail.ru>
2020-12-26 17:05:35 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import Collapse from './collapse';
+
+const collapse = new Collapse('Мой длинный-длинный, длиннющий текст');
+collapse.doButton();
---
+++ src/js/collapse.js
@@ -0,0 +1,21 @@
+export default class Collapse {
+    constructor(text) {
+        this.text = text;
+    }
+
+    doButton() {
+        const btn = document.createElement('button');
+        btn.classList.add('btn');
+        btn.textContent = 'Collapse'
+        document.body.insertAdjacentElement('beforeEnd', btn);
+        const text = document.createElement('p');
+        text.classList.add('text');
+        text.textContent = this.text;
+        btn.insertAdjacentElement('afterend', text);
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            text.classList.contains('active') ? text.classList.remove('active') : text.classList.add('active');
+            
+        })
+    }
+}
